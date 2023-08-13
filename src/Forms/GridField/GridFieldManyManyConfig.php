7cd9e8fe1d5637d95774dbb3cf31bd3118d205ef
Art Karasev <as.karasyov@gmail.com>
2023-08-13 21:28:38 +1200

    add config

---
+++ src/Forms/GridField/GridFieldManyManyConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Goldfinch\Helpers\Forms\GridField;
+
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldEditButton;
+use SilverStripe\Forms\GridField\GridField_ActionMenu;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldFilterHeader;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use SilverStripe\Forms\GridField\GridFieldSortableHeader;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+
+class GridFieldManyManyConfig extends GridFieldConfig
+{
+    public function __construct($itemsPerPage = null)
+    {
+        parent::__construct($itemsPerPage);
+
+        $this->addComponents(
+            // GridFieldFilterHeader::create(),
+            GridFieldAddNewButton::create(),
+            GridFieldAddExistingAutocompleter::create(),
+            GridFieldToolbarHeader::create(),
+            GridFieldSortableHeader::create(),
+            GridFieldDataColumns::create(),
+            GridFieldDetailForm::create(),
+            GridFieldDeleteAction::create(),
+            GridFieldEditButton::create(),
+            GridField_ActionMenu::create(),
+        );
+
+        $dataColumns = $this->getComponentByType(GridFieldDataColumns::class);
+
+        $dataColumns->setDisplayFields([
+            'Title' => 'Title',
+            'Link'=> 'URL',
+            'LastEdited' => 'Changed'
+        ]);
+
+        // $this->addComponent($dataColumns);
+
+        $this->extend('updateConfig');
+    }
+}
