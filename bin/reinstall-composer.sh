8dabb2b3ef9f66b4f3b60fee4bb9bb8e0d5a7e77
Art Karasev <as.karasyov@gmail.com>
2023-08-29 11:45:19 +1200

    add bash

---
+++ bin/reinstall-composer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf vendor/
+rm -rf composer.lock
+rm -rf public/_resources/
+composer clearcache
+composer install --ignore-platform-reqs
+php vendor/silverstripe/framework/cli-script.php dev/build "flush=1"
