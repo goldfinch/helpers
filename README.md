014834feccf7e69c29d052d39d491bdc65873ff8
Art Karasev <as.karasyov@gmail.com>
2023-09-30 00:06:19 +1300

    tweaks

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ DataObject
 - required fields
 - field descriptions
 - ShortcodeVarchar
+
+# env vars
+`APP_DEV_EMAIL`
+`APP_ERROR_LOG`
+`APP_FORCE_SSL`
+`APP_FORCE_WWW`
---
+++ src/Commands/GeneratePasswordCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Goldfinch\Helpers\Commands;
+
+use Goldfinch\Taz\Console\GeneratorCommand;
+use Goldfinch\Taz\Services\InputOutput;
+use Symfony\Component\Console\Command\Command;
+use Hackzilla\PasswordGenerator\Generator\ComputerPasswordGenerator;
+
+#[AsCommand(name: 'app:generate-password')]
+class GeneratePasswordCommand extends GeneratorCommand
+{
+    protected static $defaultName = 'app:generate-password';
+
+    protected $description = 'Generate password';
+
+    protected function execute($input, $output): int
+    {
+        // parent::execute($input, $output);
+
+        $generator = new ComputerPasswordGenerator();
+
+        $generator
+            ->setUppercase()
+            ->setLowercase()
+            ->setNumbers()
+            ->setSymbols(false)
+            ->setLength(16);
+
+        $password = $generator->generatePasswords(12);
+
+        $io = new InputOutput($input, $output);
+        $io->text($password);
+
+        return Command::SUCCESS;
+    }
+}
