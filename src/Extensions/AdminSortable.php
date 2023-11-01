2e59f3368ddf138bc9b3623f1b57d92481cecb65
Art Karasev <as.karasyov@gmail.com>
2023-11-02 09:44:51 +1300

    update admin sort

--- src/Extensions/AdminSortable.php
+++ src/Extensions/AdminSortable.php
@@ -3,12 +3,19 @@
 namespace Goldfinch\Helpers\Extensions;
 
 use SilverStripe\Core\Extension;
+use Goldfinch\Helpers\Extensions\DataObjectSortable;
 use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 class AdminSortable extends Extension
 {
     public function updateGridFieldConfig(&$config)
     {
-        $config->addComponent(GridFieldSortableRows::create('SortOrder'));
+        if (
+            in_array('SortOrder', ss_config($this->owner->modelClass, 'db')) ||
+            in_array(DataObjectSortable::class, ss_config($this->owner->modelClass, 'extensions'))
+        )
+        {
+            $config->addComponent(GridFieldSortableRows::create('SortOrder'));
+        }
     }
 }
