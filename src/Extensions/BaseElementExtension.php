4bfb4b289fac21992dff0d9759590496c52332c1
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 10:39:40 +1300

    WIP

---
+++ src/Extensions/BaseElementExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Goldfinch\Helpers\Extensions;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+class BaseElementExtension extends DataExtension
+{
+    public function updateCMSFields(FieldList $fields)
+    {
+        $title = $fields->dataFieldByName('Title');
+
+        if ($title) {
+            $title->setTitle('Block Title');
+            $fields->removeByName('Title');
+            $fields->insertBefore('ExtraClass', $title);
+        }
+
+        $global = $fields->dataFieldByName('AvailableGlobally');
+
+        $fields->insertAfter('ExtraClass', $global);
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+        if (!$this->owner->Title) {
+            $this->owner->Title = $this->owner->getType();
+        }
+    }
+
+    public function getIconClassName()
+    {
+        return $this->owner->config()->get('icon');
+    }
+}
